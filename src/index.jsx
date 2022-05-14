c04460b77e1c8e62ed8ba0873e99acff38e287b9
Idris Akintobi Adejare <akintobiidris@gmail.com>
2022-05-14 19:49:15 +0100

    Changed to jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.js";
+import App from "./App.jsx";
 import axios from "axios";
 axios.defaults.baseURL = "https://decajob-backend.herokuapp.com/api/";
 
